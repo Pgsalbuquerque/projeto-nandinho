1925424b58fcee60eafec5da2a946a113be39bbc
Pgsalbuquerque <albuquerquepgs@gmail.com>
2021-12-10 15:37:18 -0300

    10/12

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import datetime
+
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
@@ -49,6 +51,48 @@ def clearDataframe(parquetPath: str):
     columns = dataframe.columns
     trash = dataframe.to_numpy()
 
+def printColumns(parquetPath: str):
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print(dataframe.columns)
+    numpydataframe = dataframe.to_numpy()
+    counts = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for column in numpydataframe:
+        # if(np.isnan(column[0])):
+        #     counts[0] = counts[0] + 1
+        # if(np.isnan(column[1])):
+        #     counts[1] = counts[1] + 1
+        # if(np.isnan(column[2])):
+        #     counts[2] = counts[2] + 1
+        # if(np.isnan(column[3])):
+        #     counts[3] = counts[3] + 1
+        if(np.isnan(column[4])):
+            counts[4] = counts[4] + 1
+        if(np.isnan(column[5])):
+            counts[5] = counts[5] + 1
+        if(np.isnan(column[6])):
+            counts[6] = counts[6] + 1
+        if(np.isnan(column[7])):
+            counts[7] = counts[7] + 1
+        if(np.isnan(column[8])):
+            counts[8] = counts[8] + 1
+        if(np.isnan(column[9])):
+            counts[9] = counts[9] + 1
+        if(np.isnan(column[10])):
+            counts[10] = counts[10] + 1
+        if(np.isnan(column[11])):
+            counts[11] = counts[11] + 1
+        if(np.isnan(column[12])):
+            counts[12] = counts[12] + 1
+        if(np.isnan(column[13])):
+            counts[13] = counts[13] + 1
+        if(np.isnan(column[14])):
+            counts[14] = counts[14] + 1
+        if(np.isnan(column[15])):
+            counts[15] = counts[15] + 1
+        if(np.isnan(column[16])):
+            counts[16] = counts[16] + 1
+    print(counts)
+
 def printAnomalousLine(line: np.array, count: int):
     error = ""
     if line[0] == "" or line[0] == None:
@@ -89,24 +133,27 @@ def lineIsSafe(line: np.array):
         return False
     elif line[3] == "" or line[3] == None:
         return False
-    elif line[5] != 1 and line[5] != 0:
+    elif line[4] == "" or line[4] == None or np.isnan(line[4]):
         return False
-    elif line[6] != 1 and line[6] != 0:
+    elif line[5] != 1 and line[5] != 0 or np.isnan(line[5]):
         return False
-    elif line[7] != 1 and line[7] != 0:
+    elif line[6] != 1 and line[6] != 0 or np.isnan(line[6]):
         return False
-    elif line[8] != 1 and line[8] != 0:
+    elif line[7] != 1 and line[7] != 0 or np.isnan(line[7]):
         return False
-    elif line[9] != 1 and line[9] != 0:
+    elif line[8] != 1 and line[8] != 0 or np.isnan(line[8]):
         return False
-    elif line[12] != 1 and line[12] != 0:
+    elif line[9] != 1 and line[9] != 0 or np.isnan(line[9]):
         return False
-    elif line[16] != 1 and line[16] != 0:
+    elif np.isnan(line[10]):
         return False
-    
+    elif np.isnan(line[11]):
+        return False
+    elif line[12] != 1 and line[12] != 0 or np.isnan(line[12]):
+        return False
+    elif line[16] != 1 and line[16] != 0 or np.isnan(line[16]):
+        return False
     return True
-    
-    
 
 def searchAnomalies(parquetPath: str):
     print("*--abrindo database--*")
@@ -133,21 +180,116 @@ def cleannerDatabase(parquetPath: str):
     count = 0
     indexes = []
     for row in trash:
+        if row[13] != 1 and row[13] != 0 or np.isnan(row[13]):
+            row[13] = 0
+        if row[14] != 1 and row[14] != 0 or np.isnan(row[14]):
+            row[14] = 0
+        if row[15] != 1 and row[15] != 0 or np.isnan(row[15]):
+            row[15] = 0
         if(lineIsSafe(row) == False):
             indexes.append(count)
-        count += 1
+            count += 1
     print("*--deletando os itens anomalos--*")
-    print(trash[0])
-    print(trash[1])
-    print(trash[2])
-    print(trash[3])
     trash = np.delete(trash, indexes, axis=0)
-    print(len(trash))
+    print(f"*-- foram deletadas {len(indexes)} linhas --*")
     print("*--criando nova database--*")
     database = pd.DataFrame(trash, columns=columns)
     print("*--salvando a database--*")
-    database.to_parquet("teste.parquet3")
+    database.to_parquet("final.parquet3")
 
+def createDateColumn(parquetPath: str):
+    print("*--abrindo database--*")
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print("*--convertendo timestamp--*")
+    lista = []
+    dataframenumpy = dataframe.to_numpy()
+    count = 0
+    for linha in dataframenumpy:
+        lista.append(datetime.datetime.fromtimestamp(linha[4]/1000.0))
+    # datecolumn = pd.to_datetime(dataframe['timestamp'], unit="ms")
+    dataframe["local_date_time"] = lista
+    print("*--salvando a database--*")
+    dataframe.to_parquet("data.parquet3")
+
+def createPeriodDay(parquetPath: str):
+    print("*--abrindo database--*")
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print("*--convertendo timestamp--*")
+    lista = []
+    dataframenumpy = dataframe.to_numpy()
+    count = 0
+    for linha in dataframenumpy:
+        hour = linha[17].hour
+        if hour >= 0 and hour <= 4:
+            lista.append("dawn")
+        elif hour > 4 and hour < 12:
+            lista.append("morning")
+        elif hour >= 12 and hour < 19:
+            lista.append("evening")
+        elif hour >= 19 and hour <= 23:
+            lista.append("night")
+    dataframe["period_day"] = lista
+    print("*--salvando a database--*")
+    dataframe.to_parquet("data2.parquet3")
+
+def createLoginsPerId(parquetPath: str):
+    print("*--abrindo database--*")
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print("*--convertendo timestamp--*")
+    table = {}
+    dataframenumpy = dataframe.to_numpy()
+    count = 0
+    for linha in dataframenumpy:
+        if linha[1] in table.keys():
+            table[linha[1]] += 1
+        else:
+            table[linha[1]] = 1
+    dataframe = pd.DataFrame(table.items() ,columns=["account_id", "logins_amount"])
+    print("*--salvando a database--*")
+    dataframe.to_parquet("data3.parquet3")
+
+def createAccountsPerDevice(parquetPath: str):
+    print("*--abrindo database--*")
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print("*--convertendo timestamp--*")
+    tableflag = {}
+    table = {}
+    dataframenumpy = dataframe.to_numpy()
+    count = 0
+    for linha in dataframenumpy:
+        if linha[2] in tableflag.keys():
+            if not (linha[1] in tableflag[linha[2]]):
+                tableflag[linha[2]].append(linha[1])
+                table[linha[2]] += 1
+        else:
+            tableflag[linha[2]] = [linha[1]]
+            table[linha[2]] = 1
+    dataframe = pd.DataFrame(table.items() ,columns=["account_id", "logins_amount"])
+    print("*--salvando a database--*")
+    dataframe.to_parquet("data4.parquet3")
+
+def createAccountDevicesAmount(parquetPath: str):
+    print("*--abrindo database--*")
+    dataframe = pd.read_parquet(f'{parquetPath}.parquet3', engine="pyarrow")
+    print("*--convertendo timestamp--*")
+    tableflag = {}
+    table = {}
+    dataframenumpy = dataframe.to_numpy()
+    count = 0
+    for linha in dataframenumpy:
+        if linha[1] in tableflag.keys():
+            if not (linha[2] in tableflag[linha[1]]):
+                tableflag[linha[1]].append(linha[2])
+                table[linha[1]] += 1
+        else:
+            tableflag[linha[1]] = [linha[2]]
+            table[linha[1]] = 1
+    dataframe = pd.DataFrame(table.items() ,columns=["account_id", "logins_amount"])
+    print("*--salvando a database--*")
+    dataframe.to_parquet("data5.parquet3")
+
+def deleteAnomaliesLines(parquetPath: str):
+    pass
 
 def deleteAnomaliesLines(parquetPath: str):
     pass
@@ -155,6 +297,12 @@ def deleteAnomaliesLines(parquetPath: str):
 if (__name__ == "__main__"):
     # floatToBooleanColumn("logins-40", "is_emulator")
     # cleannerDatabase("logins")
-    searchAnomalies("teste")
-    
-        
+    createAccountDevicesAmount("final")
+    dataframe = pd.read_parquet(f'data5.parquet3', engine="pyarrow")
+    datanumpy = dataframe.to_numpy()
+    count = 0
+    for linha in datanumpy:
+        if (linha[1]) > 1:
+            print(linha[0] ,linha[1])
+            count += 1
+    print(f"total de {count} linhas")
